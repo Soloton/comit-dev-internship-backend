c170bb0c9b5d6799b9fa01426e92a38a70982ab0
markh <1191430+Soloton@users.noreply.github.com>
2021-09-25 20:17:01 +0300

    fix(JavaScript Sync tasks 1, 2, 3): Array instead of a generator

--- task03.js
+++ task03.js
@@ -1,10 +1,11 @@
 const catFactory = require("./task02.js");
 
-function* catsGroupGenerate(n) {
-  let index = 0;
-  while (index++ <= n) {
-    yield catFactory();
+function catsGroupGenerate(n) {
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(catFactory());
   }
+  return result;
 }
 
 module.exports = catsGroupGenerate;
