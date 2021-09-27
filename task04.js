9f84cef3b450144e9ddb00a78550bab45cd4b6a1
Mikhail Arkhipov <arkhipov-ms@ya.ru>
2021-09-27 19:28:32 +0300

    feat(js-sync-task04): Add cats names list

--- task04.js
+++ task04.js
@@ -2,4 +2,8 @@ function getMaleCats(list) {
     return list.filter(x => x.gender === 'кот');
 }
 
-module.exports = getMaleCats
+function getCatsNames(list) {
+    return list.map(x => x.name);
+}
+
+module.exports = {getMaleCats, getCatsNames}
