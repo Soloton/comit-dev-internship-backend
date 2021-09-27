da1f4b4e395b9707bbabfbdfa5dd474a4158b2a1
Mikhail Arkhipov <arkhipov-ms@ya.ru>
2021-09-27 22:55:18 +0300

    feat(js-sync-task04): Add the youngest female cats names

--- task04.js
+++ task04.js
@@ -2,6 +2,10 @@ function getMaleCats(list) {
   return list.filter((x) => x.gender === "кот");
 }
 
+function getFemaleCats(list) {
+  return list.filter((x) => x.gender === "кошка");
+}
+
 function getCatsNames(list) {
   return list.map((x) => x.name);
 }
@@ -17,4 +21,15 @@ function getOldestMaleCatsNames(list, n = Number.MAX_SAFE_INTEGER) {
   return getCatsNames(topSlice);
 }
 
-module.exports = { getMaleCats, getCatsNames, getOldestMaleCatsNames };
+function getYoungestFemaleCatsNames(list, n = Number.MAX_SAFE_INTEGER) {
+  if (n <= 0) return [];
+  const femaleCats = getFemaleCats(list);
+  const sortedFemaleCats = femaleCats.sort((a, b) => a.age - b.age);
+  const topSlice = sortedFemaleCats.slice(
+      0,
+      Math.min(list.length, n - 1)
+  );
+  return getCatsNames(topSlice);
+}
+
+module.exports = { getMaleCats, getCatsNames, getOldestMaleCatsNames, getYoungestFemaleCatsNames };
