5629fb6244c42517d01b3e60f32ed2b5b417bc39
Mikhail Arkhipov <arkhipov-ms@ya.ru>
2021-09-27 19:28:32 +0300

    feat(js-sync-task04): Add cats names list

--- entry-point.js
+++ entry-point.js
@@ -2,10 +2,10 @@ require("regenerator-runtime/runtime");
 require("@babel/register");
 
 const catsGroupGenerate = require("./task03");
-const getMaleCats = require("./task04");
+const task04 = require("./task04");
 
 const list = catsGroupGenerate(999);
 
-for (let cat of getMaleCats(list)) {
+for (let cat of task04.getCatsNames(list)) {
   console.log(cat);
 }
