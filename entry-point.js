7e3ebc28f691747210c6001f55dba832f13e24fd
Mikhail Arkhipov <arkhipov-ms@ya.ru>
2021-09-27 23:54:50 +0300

    feat(js-sync-task06): Add optional cat object with default values

--- entry-point.js
+++ entry-point.js
@@ -4,6 +4,7 @@ require("@babel/register");
 const catsGroupGenerate = require("./task03");
 const nameStats = require("./task05");
 
-const list = catsGroupGenerate(999);
+const list = catsGroupGenerate(999, {name: 'Нехочуха'});
 
 console.log(nameStats(list));
+
