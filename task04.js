ff3fc0f62c269c120c3c0f915283b35a1daa9dac
Mikhail Arkhipov <arkhipov-ms@ya.ru>
2021-09-27 21:27:01 +0300

    feat(js-sync-task04): Add oldest male cats names

--- task04.js
+++ task04.js
@@ -1,9 +1,20 @@
 function getMaleCats(list) {
-    return list.filter(x => x.gender === 'кот');
+  return list.filter((x) => x.gender === "кот");
 }
 
 function getCatsNames(list) {
-    return list.map(x => x.name);
+  return list.map((x) => x.name);
 }
 
-module.exports = {getMaleCats, getCatsNames}
+function getOldestMaleCatsNames(list, n = Number.MAX_SAFE_INTEGER) {
+  if (n <= 0) return [];
+  const maleCats = getMaleCats(list);
+  const descendingSortedMaleCats = maleCats.sort((a, b) => b.age - a.age);
+  const topSlice = descendingSortedMaleCats.slice(
+    0,
+    Math.min(list.length, n - 1)
+  );
+  return getCatsNames(topSlice);
+}
+
+module.exports = { getMaleCats, getCatsNames, getOldestMaleCatsNames };
